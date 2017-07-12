6f00bedb72722385ecc4a5f8ea2c8a9b5bc4692d
Julius Bacosa <juliusbacosa@gmail.com>
2017-07-12 09:57:29 +0800

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MdButtonModule, MdCheckboxModule, MdListModule, MdTabsModule, MdChipsModule, MdProgressBarModule, MdDialogModule} from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, BrowserAnimationsModule, MdButtonModule, MdCheckboxModule, MdListModule, MdTabsModule, MdChipsModule,MdProgressBarModule,MdDialogModule ],
+  exports: [MdButtonModule, MdCheckboxModule,MdListModule, MdTabsModule, MdChipsModule, MdProgressBarModule, MdDialogModule],
   providers: [],
   bootstrap: [AppComponent]
 })
